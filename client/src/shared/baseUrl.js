df01262deb0a5aeb220b97a6ea7f03b996a459c3
editarosanna <editarosanna5@gmail.com>
2020-10-04 03:10:05 +0700

    changed node baseUrl to 127.0.0.1

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -1,8 +1,11 @@
 // export const baseUrl = 'http://192.168.100.10:5000/';
 // export const reactBaseUrl = 'http://192.168.100.10:3000/';
 
-export const baseUrl = 'http://localhost:5000/';
+// export const baseUrl = 'http://localhost:5000/';
 // export const reactBaseUrl = 'http://localhost:3000/';
+
+export const baseUrl = 'http://127.0.0.1:5000/';
+// export const reactBaseUrl = 'http://127.0.0.1:3000/';
 
 // export const baseUrl = 'http://172.31.56.178:5000/';
 export const reactBaseUrl = 'http://172.31.56.178:3000/';
