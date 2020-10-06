64fce581548ed4fc40aa0f431a187d7831a55300
editarosanna <editarosanna5@gmail.com>
2020-10-07 03:41:35 +0700

    refresh test 7

--- client/src/components/LocationDetailComponent.js
+++ client/src/components/LocationDetailComponent.js
@@ -32,8 +32,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        // this.props.postComment(this.props.locationId,values.rating,values.author,values.comment);
-        // this.props.returnedComm(this.props.locationId.comments);
+        postComment(this.props.locationId,values.rating,values.author,values.comment);
     }
 
     render(){
