91ee75f41ef75ed467211f8865aa6f29439a76d9
editarosanna <editarosanna5@gmail.com>
2020-10-06 23:44:28 +0700

    deployment without nginx test 4

--- client/src/shared/baseUrl.js
+++ client/src/shared/baseUrl.js
@@ -2,7 +2,7 @@
 // export const reactBaseUrl = 'http://192.168.100.10:3000/';
 
 // export const baseUrl = 'http://localhost:5000/';
-export const reactBaseUrl = 'http://localhost:3000/';
+// export const reactBaseUrl = 'http://localhost:3000/';
 
 // export const baseUrl = 'http://127.0.0.1:5000/';
 // export const reactBaseUrl = 'http://127.0.0.1:3000/';
@@ -11,4 +11,4 @@ export const reactBaseUrl = 'http://localhost:3000/';
 // export const reactBaseUrl = 'http://54.175.51.78/';
 
 export const baseUrl = 'http://54.175.51.78:5000/';
-// export const reactBaseUrl = 'http://54.175.51.78:3000/';
+export const reactBaseUrl = 'http://54.175.51.78:3000/';
