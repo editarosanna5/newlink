ddd9528d2a069b1375bcbcb2c7f789ee3afe0a09
editarosanna <editarosanna5@gmail.com>
2020-10-07 01:06:59 +0700

    display test 2

--- client/src/components/MenuComponent.js
+++ client/src/components/MenuComponent.js
@@ -3,14 +3,13 @@ import { Card, CardImg, CardImgOverlay, CardTitle, Input} from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
-import { reactBaseUrl } from '../shared/baseUrl';
 import Header from './HeaderComponent';
 
 function RenderMenuItem ({location}){
     return (
         <Card>
             <Link to={`/home/${location._id}`} >
-                <CardImg width="100%" src={baseUrl + 'images/locations/' + location.image + '.PNG'} alt={location.name} />
+                <CardImg width="100%" src={baseUrl + location.image} alt={location.name} />
                 <CardImgOverlay>
                     <CardTitle style={{color:"white"}}>{location.name}</CardTitle>
                 </CardImgOverlay>
