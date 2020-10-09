1059516cdb913a7bec2ee292ca54eeaa0d643e36
Louis Tan <louistannn@gmail.com>
2020-10-09 14:41:57 +0700

    update how to use component.js

--- client/src/components/HowToUseComponent.js
+++ client/src/components/HowToUseComponent.js
@@ -18,36 +18,36 @@ function HowToUse() {
                 <div className="col-12 col-md-6" >
                     <Media tag="li">
                         <Media left middle>
-                            <Media object src="assets/images/Logo.png" alt="Step 1" id="howtouseComp"/>
+                            <Media object src="assets/images/Logo.png" alt= "Langkah 1" id="howtouseComp"/>
                         </Media>
                         <Media body className="ml-5 steps">
                             <Media heading id="mediaHeading">
-                                <img src={reactBaseUrl + "assets/images/number-1.png"} alt="one" id="imgNumber"/>
-                                Step 1
+                                <img src={reactBaseUrl + "assets/images/egg.gif"} alt="one" id="imgNumber"/>
+                                Langkah 1
                             </Media>
                             <p id="paraContent">Buka aplikasi NewLink. Tampilan beranda akan muncul pertama kali Anda mengakses aplikasi NewLink</p>
                         </Media>
                     </Media>    
                     <Media tag="li">
                         <Media left middle>
-                            <Media object src="assets/images/Logo.png" alt="Step 2" id="howtouseComp"/>
+                            <Media object src="assets/images/Logo.png" alt="Langkah 2" id="howtouseComp"/>
                         </Media>
                         <Media body className="ml-5">
                             <Media heading id="mediaHeading">
                                 <img src={reactBaseUrl + "assets/images/number-2.png"} alt="two" id="imgNumber"/>
-                                Step 2
+                                Langkah 2
                             </Media>
                             <p id="paraContent">Anda dapat melakukan pencarian mal pada halaman beranda atau dapat melakukan pencarian dengan menggunakan bilah pencarian.</p>
                         </Media>
                     </Media>
                     <Media tag="li">
                         <Media left middle>
-                            <Media object src="assets/images/Logo.png" alt="Step 3" id="howtouseComp"/>
+                            <Media object src="assets/images/Logo.png" alt="Langkah 3" id="howtouseComp"/>
                         </Media>
                         <Media body className="ml-5 steps">
                             <Media heading id="mediaHeading">
                                 <img src={reactBaseUrl + "assets/images/number-3.png"} alt="three" id="imgNumber"/>
-                                Step 3
+                                Langkah 3
                             </Media>
                             <p id="paraContent">Klik pada mal yang ingin dikunjungi dan akses informasi yang tersedia untuk mengetahui kapasitas dari mal tersebut.</p>
                         </Media>
